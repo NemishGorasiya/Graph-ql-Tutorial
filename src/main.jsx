c786e8959204ed67d2601d1ff1f5f03eb4a9c576
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-07-05 01:42:15 +0530

    fix: bug in cache update

--- src/main.jsx
+++ src/main.jsx
@@ -5,40 +5,36 @@ import App from "./App.jsx";
 import "./index.css";
 
 const client = new ApolloClient({
-  uri: "https://graphql.anilist.co",
-  cache: new InMemoryCache({
-    addTypename: true,
-    typePolicies: {
-      Page: {
-        fields: {
-          media: {
-            keyArgs: false,
-            merge(existing = [], incoming) {
-              console.log("existing", existing);
-              console.log("incoming", incoming);
-              if (!incoming) return existing;
-              if (!existing) return incoming;
-              return [...existing, ...incoming];
-            },
-            read(existing, { args }) {
-              console.log("args", args);
-              console.log("existing", existing);
-            },
-          },
-        },
-        read(existing, { args }) {
-          console.log("args", args);
-          console.log("existing", existing);
-        },
-      },
-    },
-  }),
+	uri: "https://graphql.anilist.co",
+	cache: new InMemoryCache({
+		typePolicies: {
+			Query: {
+				fields: {
+					Page: {
+						keyArgs: false,
+						merge: (existing = {}, incoming) => {
+							if (!existing) {
+								return incoming;
+							}
+
+							let merged = { ...incoming };
+							merged["media({})"] = existing["media({})"]
+								? [...existing["media({})"], ...incoming["media({})"]]
+								: [...incoming["media({})"]];
+							console.log("merged", merged);
+							return merged;
+						},
+					},
+				},
+			},
+		},
+	}),
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>
 );
